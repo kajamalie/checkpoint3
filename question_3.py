cb8d92d190122255fcc0197e2130f7df6aff6acc
kajamalie <test>
2020-08-21 11:32:51 +0200

    ferdig 3

---
+++ question_3.py
@@ -0,0 +1,35 @@
+#The company SuperSale3000 want to rank its sales team at the end of every week. 
+#Each call gives the seller 10 points, each meeting 30 points and each sale 100 points.
+# There is also a 100-point bonus for each, if you make more than 150 calls, or 20 meetings or 5 sales.
+#Create a new file called “question_3.py”.
+#They already have the data for each employee saved in a dictionary.
+#At the top of the file set the following example dictionary for Jodan Belfort:
+#Jordan_belfort = {‘calls’: 178, ‘meetings’:17,  ‘sales’:6}
+#Add the following logic to calculate the total score from the dictionary and add it to the same dictionary 
+#using the key ‘score’
+
+Jordan_belfort = {'calls': 178, 'meetings':17,  'sales':6}
+
+def rank_sale(dic):
+    bonus = {'points': 0}
+    for calls in dic:
+        bonus['points'] = + dic['calls'] * 10
+        if dic['calls'] > 150:
+            bonus['points'] = (bonus['points']) + 100
+        else:
+            pass
+        for meetings in dic:
+            bonus['points'] = (bonus['points']) + dic['meetings'] * 30
+            if dic['meetings'] > 20:
+                bonus['points'] = (bonus['points']) + 100
+            else:
+                pass
+            for sales in dic:
+                bonus['points'] = (bonus['points']) + dic['sales'] * 100
+                if dic['sales'] > 5:
+                    bonus['points'] = (bonus['points']) + 100
+                    
+            
+    print(bonus) 
+    
+  
